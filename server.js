fb95c6dfff0dede1ba798351e3d8e498c24a90c7
Usama-Ashraf27 <usama@karigar.pk>
2024-11-01 07:25:59 -0700

    be

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
-const adminOrderRouter = require("./routes/admin/order-routes");
 const adminCategoryRouter = require("./routes/admin/category-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
 
-const shopCartRouter = require("./routes/shop/cart-routes");
-const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
-const shopReviewRouter = require("./routes/shop/review-routes");
 
 const commonFeatureRouter = require("./routes/common/feature-routes");
-const shopinquiryRouter = require("./models/inquiry");
-const inquiry = require("./models/inquiry");
 
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file here
@@ -50,15 +44,10 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
-// app.use("/api/admin/orders", adminOrderRouter);
 app.use("/api/admin/category", adminCategoryRouter);
-// app.use("/api/shop/inquiry", inquiry);
 app.use("/api/shop/products", shopProductsRouter);
-// app.use("/api/shop/cart", shopCartRouter);
-// app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
-// app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
 
