f5ca930b5542cb497bba81a3a229cb0ee71dfbea
Usama-Ashraf27 <usama@karigar.pk>
2024-11-08 03:53:01 -0800

    fix origin

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://helpful-twilight-3366e0.netlify.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
